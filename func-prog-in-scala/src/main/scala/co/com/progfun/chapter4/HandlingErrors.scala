1f7f19d2664274280d2cb94e9982b4e87c44e639
julianGonzalezV <j.andres2087@gmail.com>
2018-07-12 22:34:26 -0500

    Option[T]

--- func-prog-in-scala/src/main/scala/co/com/progfun/chapter4/HandlingErrors.scala
+++ func-prog-in-scala/src/main/scala/co/com/progfun/chapter4/HandlingErrors.scala
@@ -21,25 +21,28 @@ Option[Animal]).
     def filter(f: A => Boolean): Option[A]
   }
   case class Some[+A](get: A) extends Option[A] {
-    override def map[B](f: A => B): Option[B] = ???
+    override def map[B](f: A => B): Option[B] = Some(f(get))
 
-    override def flatMap[B](f: A => Option[B]): Option[B] = ???
+    override def flatMap[B](f: A => Option[B]): Option[B] = f(get)
 
-    override def getOrElse[B >: A](default: => B): B = ???
+    override def getOrElse[B >: A](default: => B): B = get
 
-    override def orElse[B >: A](ob: => Option[B]): Option[B] = ???
+    override def orElse[B >: A](ob: => Option[B]): Option[B] = Some(get)
 
-    override def filter(f: A => Boolean): Option[A] = ???
+    override def filter(f: A => Boolean): Option[A] = {
+      if(f(get)) Some(get) else None
+    }
 }
+
   case object None extends Option[Nothing] {
-    override def map[B](f: Nothing => B): Option[B] = ???
+    override def map[B](f: Nothing => B): Option[B] = None
 
-    override def flatMap[B](f: Nothing => Option[B]): Option[B] = ???
+    override def flatMap[B](f: Nothing => Option[B]): Option[B] = None
 
-    override def getOrElse[B >: Nothing](default: => B): B = ???
-    override def orElse[B >: Nothing](ob: => Option[B]): Option[B] = ???
+    override def getOrElse[B >: Nothing](default: => B): B = default
+    override def orElse[B >: Nothing](ob: => Option[B]): Option[B] = ob
 
-    override def filter(f: Nothing => Boolean): Option[Nothing] = ???
+    override def filter(f: Nothing => Boolean): Option[Nothing] = None
 }
 
   //la idea es gestion de errores sin lanzar excepciones
@@ -135,8 +138,49 @@ Option[Animal]).
     println(HandlingErrors.meanOk(Seq(3,6,9)))
     println(HandlingErrors.meanOk(Nil))
 
+    println("Some test::::::::::::::::::")
+    println(HandlingErrors.Some(5).map(x=> x +3))
+    val someSome= Some(Some(3))
+    println(someSome)
+    println("Map::::::::::::::::::")
+    println(someSome.map(x =>  x))
+    println("FlatMap:lo aplana :O:::::::::::::::::")
+    println(someSome.flatMap(x =>  x))
+    println(HandlingErrors.meanOk(Nil))
 
+    println("getOrElse::::::::::::::::::")
+    println(Some(5).getOrElse(2))
+    println(None.getOrElse(2))
 
+    println("orElse::::::::::::::::::")
+    println(Some(5).orElse(Some(2)))
+    println(None.orElse(Some(2)))
+
+    println("Employee exAMPLE::::::::::::::::::")
+    case class Employee(name: String, department: String){
+      def otroDepartamento():String ={
+        println("entra a la funcion")
+        "Tulua"
+      }
+    }
+    val employeesByName: Map[String, Employee] =
+      List(Employee("Alice", "R&D"), Employee("Bob", "Accounting")).
+        map(e => (e.name, e)).toMap
+    println(employeesByName)
+    //note como ya el None se aplica acá, el .get de un Map devuelve un Option
+    //note lo potente de no validar qque si employeesByName.get("Joe") != null etc etc
+    val dept1: scala.Option[String] = employeesByName.get("Joe").map(_.department)
+    println(dept1)
+    val dept2: scala.Option[String] = employeesByName.get("Bob").map(_.department)
+    println(dept2)
+
+    //Este ejemplo es más bacano, mire como de una cuando no na nada, es decir None en
+    //get("joe") NO EJECUTA EL MAP Y NI SIUQIERA LLAMA A otroDepartamento y no sacó Null Poiter Exception
+    // Te suena familar :)
+    val dept3: scala.Option[String] = employeesByName.get("Joe").map(_.otroDepartamento())
+    println(dept3)
+    val dept4: scala.Option[String] = employeesByName.get("Bob").map(_.otroDepartamento())
+    println(dept4)
 
   }
 
--- func-prog-in-scala/src/main/scala/co/com/progfun/miscellaneous/traitEg/MainTr.scala
+++ func-prog-in-scala/src/main/scala/co/com/progfun/miscellaneous/traitEg/MainTr.scala
@@ -5,5 +5,6 @@ object MainTr extends App {
   val  itr1: Iterator[Int] = new IntIterator(10)
   println(itr1.next)
   println(itr1.next)
+  Some(4)
 
 }
