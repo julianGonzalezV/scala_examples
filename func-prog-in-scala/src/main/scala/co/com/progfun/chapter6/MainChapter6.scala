78e8a191c5196dacabf729b1afab36d8831471ee
julianGonzalezV <j.andres2087@gmail.com>
2018-08-12 16:44:06 -0500

    chapter 6

--- func-prog-in-scala/src/main/scala/co/com/progfun/chapter6/MainChapter6.scala
+++ func-prog-in-scala/src/main/scala/co/com/progfun/chapter6/MainChapter6.scala
@@ -24,7 +24,27 @@ object MainChapter6 extends App{
   }
 
 
+  /**
+    * Que devuelve random? R Un númnero , por lo cual lo que hacemos es  que ése numero lo devuelva en una tupla en elprimer elemento y
+    * para calcular el siguiente estado de devulve el objeto que calcula el random en el segundo elemento de la tupla
+    * A continuación el ejemplo:
+    *
+    *
+    * En la vida real nos podemos encontrar con
+    * class Foo {
+private var s: FooState = ...
+def bar: Bar
+def baz: Int
+}//en donde posiblemente las funciones bar o baz alteren en estado de var s
 
+    por lo cual el patron que se aplica es el mismo que el aplicado en random(tupla en donde el primer elemento es (ver arriba))
+    solución:
+    trait Foo {
+def bar: (Bar, Foo)
+def baz: (Int, Foo)
+}
+
+    */
 
   //Implementando mi proipio random generator, pero funcioalmente puro
 
@@ -45,5 +65,16 @@ object MainChapter6 extends App{
   println(rd1._2.nextInt)
 
 
+  println(":::::::::nonNegativeInt(rng: RNG):::::::::::::::::::")
+  val rng2: RNG = SimpleRNG(10)
+  val nn1 = rng2.nonNegativeInt(rng2)
+  val nn2 = nn1._2.nonNegativeInt(nn1._2)
+  println(nn1._1.toDouble)
+  println(nn2)
 
+  println(":::::::::double(rng: RNG):::::::::::::::::::")
+  val db1 = rng2.double(rng2)
+  val db2 = db1._2.double(db1._2)
+  println(db1)
+  println(db2)
 }
--- func-prog-in-scala/src/main/scala/co/com/progfun/chapter6/RNG.scala
+++ func-prog-in-scala/src/main/scala/co/com/progfun/chapter6/RNG.scala
@@ -2,6 +2,16 @@ package co.com.progfun.chapter6
 
 trait RNG {
   def nextInt: (Int, RNG)
+
+  def nonNegativeInt(rng: RNG): (Int, RNG)
+
+  def double(rng: RNG): (Double, RNG)
+
+  def intDouble(rng: RNG): ((Int,Double), RNG)
+
+  def doubleInt(rng: RNG): ((Double,Int), RNG)
+
+  def double3(rng: RNG): ((Double,Double,Double), RNG)
 }
 
 case class SimpleRNG(seed:Long) extends RNG {
@@ -11,6 +21,24 @@ case class SimpleRNG(seed:Long) extends RNG {
     val n = (newSeed >>> 16).toInt
       (n, nextRNG)
   }
+
+  override def nonNegativeInt(rng: RNG): (Int, RNG) = {
+    val v1: (Int, RNG) = rng.nextInt
+    (math.abs(v1._1), v1._2)
+  }
+
+  override def double(rng: RNG) = {
+    val (result, nextState) = nonNegativeInt(rng)
+    (math.abs(result)/(Int.MaxValue.toDouble+0.1), nextState)
+  }
+
+  override def intDouble(rng: RNG): ((Int,Double), RNG) = {
+
+  }
+
+  override def doubleInt(rng: RNG) = ???
+
+  override def double3(rng: RNG) = ???
 }
 
 case class JulianoRNG(seed:Long) extends RNG {
@@ -20,4 +48,14 @@ case class JulianoRNG(seed:Long) extends RNG {
     val n = (newSeed + 2).toInt
     (n, nextRNG)
   }
+
+  override def nonNegativeInt(rng: RNG) = ???
+
+  override def double(rng: RNG) = ???
+
+  override def intDouble(rng: RNG) = ???
+
+  override def doubleInt(rng: RNG) = ???
+
+  override def double3(rng: RNG) = ???
 }
