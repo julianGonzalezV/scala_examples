1599907d63ae315674ff96dd723843857a9e9da6
julianGonzalezV <j.andres2087@gmail.com>
2018-09-04 17:23:26 -0500

    fix

--- func-prog-in-scala/src/main/scala/co/com/progfun/chapter7/MainChapter7.scala
+++ func-prog-in-scala/src/main/scala/co/com/progfun/chapter7/MainChapter7.scala
@@ -1,5 +1,7 @@
 package co.com.progfun.chapter7
 
+import co.com.progfun.chapter7.Par.Par
+
 object MainChapter7 {
 
   /**
@@ -31,18 +33,19 @@ object MainChapter7 {
 
    */
 
+  /*
   def sumParaV1(ints: IndexedSeq[Int]): Int ={
     if (ints.size <= 1) ints.headOption getOrElse  0
     else{
       val (left, right) = ints.splitAt(ints.length/2)
-      val sumL = Par.unit(sumParaV1(left))
-      val sumR = Par.unit(sumParaV1(right))
-      val v1 = Par.run(sumL)
+      val sumL: Par[Int] = Par.unit(sumParaV1(left))
+      val sumR: Par[Int] = Par.unit(sumParaV1(right))
+      val v1 = (s:ExecutorService) => Par.run(s)(sumL)
 
       Par.run(sumL).get() + Par.run(sumR).get()
     }
   }
-
+*/
 
   def main(args: Array[String]): Unit = {
     println(sum(IndexedSeq(1,2,3)))
--- func-prog-in-scala/src/main/scala/co/com/progfun/chapter7/Par.scala
+++ func-prog-in-scala/src/main/scala/co/com/progfun/chapter7/Par.scala
@@ -18,131 +18,131 @@ object Par {
     * @tparam B
     * @return
     */
-  def asyncF[A,B](f: A => B): A => Par[B] =
-    a => {
-      lazyUnit(a)
-    }
+  def asyncF[A,B](f: A => B): A => Par[B] = {
+    a => lazyUnit(f(a))
 
-
-  type Par[A] = ExecutorService => Future[A]
-
-  //V1: Se llamaba get: obtiene el resultado de la computación paralela
-  //V2 se llama run para indicar que acá es donde realmente se implementa el paralelismo
-  def run[A](s: ExecutorService)(a: Par[A]): Future[A] =a(s) // note como le pasa a "a" el ExecutorService
-
-  /**
-    * Combina el resultado de 2 operaciones paralelas , aplicando una funcion binaria(que recibe 2 parámetros)
-    *
-    * @param a
-    * @param b
-    * @param f
-    * @tparam A
-    * @return
-    */
-  def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] = {
-    (es: ExecutorService) => {
-      val af: Future[A] = a(es)
-      val bf: Future[B] = b(es)
-      UnitFuture(f(af.get, bf.get))
-    }
   }
+      type Par[A] = ExecutorService => Future[A]
 
-  /**
-    * La idea de esa función como ejemplo es que dada una lista de Ints envuelta en un Par
-    * devolver otra lista ordenada y también envuelta en un Par , opciones de solucion
-    * 1) Por medio de la función run (arriba) Par.run (hacer run del par) obtener la lista, ordenarla
-    * y envolverla de nuevo en otro Par.Pero que si queremos evitar ejecutar run??(entonces vaya al punto 2)
-    *
-    * 2) Es precisamente mediante map2, así como se implemente a continuación. En donde ya que map2 permite la manipulación del
-    * value of a Par, apicandole una función (ver la implementación de map2 arriba)
-    * @param parList
-    * @return
-    */
-  def sortPar(parList: Par[List[Int]]): Par[List[Int]] =
-    map2(parList, unit(()))((a, _) => a.sorted)
+      //V1: Se llamaba get: obtiene el resultado de la computación paralela
+      //V2 se llama run para indicar que acá es donde realmente se implementa el paralelismo
+      def run[A](s: ExecutorService)(a: Par[A]): Future[A] =a(s) // note como le pasa a "a" el ExecutorService
 
+      /**
+        * Combina el resultado de 2 operaciones paralelas , aplicando una funcion binaria(que recibe 2 parámetros)
+        *
+        * @param a
+        * @param b
+        * @param f
+        * @tparam A
+        * @return
+        */
+      def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] = {
+        (es: ExecutorService) => {
+          val af: Future[A] = a(es)
+          val bf: Future[B] = b(es)
+          UnitFuture(f(af.get, bf.get))
+        }
+      }
 
-  /**
-    * Lifting any function of type A => B
-    * to become a function that takes Par[A] and returns Par[B]
-    * @param pa
-    * @param f
-    * @tparam A
-    * @tparam B
-    * @return
-    */
-  def map[A,B](pa: Par[A])(f: A => B): Par[B] =
-    map2(pa, unit(()))((a,_) => f(a))
-
-  /**
-    * Con el lifting anterior entonces podemos volver a escribir la función sortPar implementada anteriormente
-    * @param parList
-    * @return
-    */
-  def sortParV2(parList: Par[List[Int]]) = map(parList)(_.sorted)
-
-
-
-
-  /**
-    * Funcion ára combinar N computaciones paralelas
-    * I) Remember that Par[A] is simply an alias for ExecutorService => Future[A]
-    *
-    * fbs es
-    * @param ps
-    * @param f
-    * @tparam A
-    * @tparam B
-    * @return
-    */
-  def parMap[A,B](ps: List[A])(f: A => B): Par[List[B]] = {
-    //fbs es el val para indicar el la función aplicada a A para retornar el listado de Bs
-    val fbs: List[Par[B]] = ps.map(asyncF(f))
-    sequence(fbs)
-  }
+      /**
+        * La idea de esa función como ejemplo es que dada una lista de Ints envuelta en un Par
+        * devolver otra lista ordenada y también envuelta en un Par , opciones de solucion
+        * 1) Por medio de la función run (arriba) Par.run (hacer run del par) obtener la lista, ordenarla
+        * y envolverla de nuevo en otro Par.Pero que si queremos evitar ejecutar run??(entonces vaya al punto 2)
+        *
+        * 2) Es precisamente mediante map2, así como se implemente a continuación. En donde ya que map2 permite la manipulación del
+        * value of a Par, apicandole una función (ver la implementación de map2 arriba)
+        * @param parList
+        * @return
+        */
+      def sortPar(parList: Par[List[Int]]): Par[List[Int]] =
+        map2(parList, unit(()))((a, _) => a.sorted)
 
 
-  /**
-    * La implementación es la misma que la del chapter 4 con Option
-    * HandlingErrors.scala
-    * @param ps
-    * @tparam A
-    * @return
-    */
-  def sequence[A](ps: List[Par[A]]): Par[List[A]] = {
-    def loop[A](li: List[Par[A]], acc:  Par[List[A]): Par[List[A]] ={
-      if(li.isEmpty) acc
-      else loop(li.tail, map2(acc, li.head)((x,y)=> x :+ y))
-    }
-    loop(ps, Par[List])
-  }
+      /**
+        * Lifting any function of type A => B
+        * to become a function that takes Par[A] and returns Par[B]
+        * @param pa
+        * @param f
+        * @tparam A
+        * @tparam B
+        * @return
+        */
+      def map[A,B](pa: Par[A])(f: A => B): Par[B] =
+        map2(pa, unit(()))((a,_) => f(a))
 
-  /**
-    * Filtrar los elementos de una lista en paralelo
-    * @param as
-    * @param f
-    * @tparam A
-    * @return
-    */
-  def parFilter[A](as: List[A])(f: A => Boolean): Par[List[A]] = {
-    val v1 = as.filter(a => a)
-  }
+      /**
+        * Con el lifting anterior entonces podemos volver a escribir la función sortPar implementada anteriormente
+        * @param parList
+        * @return
+        */
+      def sortParV2(parList: Par[List[Int]]) = map(parList)(_.sorted)
 
-  //a diferencia de unit acá marca "a"  para ser ejecutada concurrentemente
-  /**
-    * Esta es la implementación más sencilla pero:
-    * el new Callable[A]{..} se bloquea hasta que la operación que
-    * está dentro de {..} termine
-    * @param a
-    * @tparam A
-    * @return
-    */
-  def fork[A](a: => Par[A]): Par[A] = {
-    es =>
-      es.submit(new Callable[A] {
-        def call = a(es).get
-      })
-  }
+
+
+
+      /**
+        * Funcion ára combinar N computaciones paralelas
+        * I) Remember that Par[A] is simply an alias for ExecutorService => Future[A]
+        *
+        * fbs es
+        * @param ps
+        * @param f
+        * @tparam A
+        * @tparam B
+        * @return
+        */
+      def parMap[A,B](ps: List[A])(f: A => B): Par[List[B]] = {
+        //fbs es el val para indicar el la función aplicada a A para retornar el listado de Bs
+        val fbs: List[Par[B]] = ps.map(asyncF(f))
+        sequence(fbs)
+      }
+
+
+      /**
+        * La implementación es la misma que la del chapter 4 con Option
+        * HandlingErrors.scala
+        * @param ps
+        * @tparam A
+        * @return
+        */
+      def sequence[A](ps: List[Par[A]]): Par[List[A]] = {
+        def loop[A](li: List[Par[A]], acc:  Par[List[A]]): Par[List[A]] ={
+          if(li.isEmpty) acc
+          else loop(li.tail, map2(acc, li.head)((x,y)=> x :+ y))
+        }
+        loop(ps, Par[List])
+      }
+
+      /**
+        * Filtrar los elementos de una lista en paralelo
+        * @param as
+        * @param f
+        * @tparam A
+        * @return
+        */
+      def parFilter[A](as: List[A])(f: A => Boolean): Par[List[A]] = {
+        val v1: List[A] = as.filter(elem => f(elem))
+        Par(v1)
+      }
+
+      //a diferencia de unit acá marca "a"  para ser ejecutada concurrentemente
+      /**
+        * Esta es la implementación más sencilla pero:
+        * el new Callable[A]{..} se bloquea hasta que la operación que
+        * está dentro de {..} termine
+        * @param a
+        * @tparam A
+        * @return
+        */
+      def fork[A](a: => Par[A]): Par[A] = {
+        es =>
+          es.submit(new Callable[A] {
+            def call = a(es).get
+          })
+      }
+
 }
 
 private case class UnitFuture[A](get: A) extends Future[A] {
